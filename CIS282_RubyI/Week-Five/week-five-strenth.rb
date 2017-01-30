aa2acc68ef5dff79f88e5316d13240169b493b30
Dennis Sauve <dengsauve@yahoo.com>
2017-01-30 07:43:50 -0800

    Created week five strength for later use

---
+++ CIS282_RubyI/Week-Five/week-five-strenth.rb
@@ -0,0 +1,10 @@
+############################################################
+#
+#  Name:         Dennis Sauve
+#  Date:         01/30/17
+#  Assignment:   Week Five Strength Exercises
+#  Class:        CIS282
+#  Description:  The Week Five Strength Exercises from the book.
+#
+############################################################
+
