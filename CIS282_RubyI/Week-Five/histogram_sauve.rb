25439d5efea0f8de837aeb8b5f4a7be212dbb3ff
Dennis <dengsauve@yahoo.com>
2017-01-30 13:19:24 -0800

    Completed Histogram 1 line

--- CIS282_RubyI/Week-Five/histogram_sauve.rb
+++ CIS282_RubyI/Week-Five/histogram_sauve.rb
@@ -8,8 +8,4 @@
 #
 ############################################################
 
-histogram = [0,0,0,0,0,0,0,0,0,0]
-(Array.new(200) {rand(1..200)}).each {|i| histogram[i%10] += 1}
-puts 'Range'.ljust(7) + "\#Found".center(12) + 'Chart'.ljust(20) + "\n" + '-----'.ljust(7) + '------'.center(12) + '-----'.ljust(20)
-histogram.each_with_index {|i, index| puts "#{10*index+1}".ljust(2) + " - #{10*index+10}".ljust(6) + "| #{i.to_s} |".rjust(8) + "   #{'*' * i}"}
-
+((histogram = [0,0,0,0,0,0,0,0,0,0]).tap {(Array.new(200) {rand(1..100)}).each {|i| histogram[(i-1)/10] += 1}}).each_with_index {|i, index| puts "#{puts 'Range'.ljust(7) + "\#Found".center(12) + 'Chart'.ljust(20) + "\n" + '-----'.ljust(7) + '------'.center(12) + '-----'.ljust(20) if index==0}#{10*index+1}".ljust(2) + " - #{10*index+10}".ljust(6) + "| #{i.to_s} |".rjust(8) + "   #{'*' * i}"}
