5a11ad10b79efe1087ecb6c8d3b0b14892a5739d
Dennis Sauve <dengsauve@yahoo.com>
2017-02-13 08:23:25 -0800

    Started on Hash project

---
+++ CIS282_RubyI/Week-Seven/Week-Seven-Project-Hashes/hash_sauve.rb
@@ -0,0 +1,18 @@
+menu = 'Welcome User
+  1. View all Products
+  2. Add a Product
+  3. Remove a Product
+  4. Update a Product
+  5. View Highest Priced Product
+  6. View Lowest Priced Product
+  7. View Sum of all Product Prices
+
+  8. Exit
+'
+
+puts menu
+
+# new_num = rand(100..999)
+# while product.has_key?(new_num)
+#   new_num = rand(100..999)
+# end
