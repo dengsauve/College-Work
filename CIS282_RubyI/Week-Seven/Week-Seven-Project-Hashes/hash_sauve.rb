6da43bd7e9ef01bc05cd2816869af3c3e51f8c67
Dennis <dengsauve@yahoo.com>
2017-02-14 10:41:49 -0800

    Laid out blocks for Hash Assignment

--- CIS282_RubyI/Week-Seven/Week-Seven-Project-Hashes/hash_sauve.rb
+++ CIS282_RubyI/Week-Seven/Week-Seven-Project-Hashes/hash_sauve.rb
@@ -1,3 +1,14 @@
+############################################################
+#
+#  Name:         Dennis Sauve
+#  Date:         02/14/17
+#  Assignment:   Hash Assignment
+#  Class:        CIS282
+#  Description:  Assignment to create an inventory control
+#                system for a retail business
+#
+############################################################
+
 menu = 'Welcome User
   1. View all Products
   2. Add a Product
@@ -10,9 +21,71 @@ menu = 'Welcome User
   8. Exit
 '
 
-puts menu
+def view_products (inventory)
 
-# new_num = rand(100..999)
-# while product.has_key?(new_num)
-#   new_num = rand(100..999)
-# end
+end
+
+def add_product (inventory)
+  new_num = rand(100..999)
+  while inventory.has_key?(new_num)
+    new_num = rand(100..999)
+  end
+end
+
+def remove_product (inventory)
+
+end
+
+def update_product (inventory)
+
+end
+
+def highest_priced (inventory)
+
+end
+
+def lowest_priced (inventory)
+
+end
+
+def product_sum (inventory)
+
+end
+
+def startup
+  f = File.open('product.txt')
+  product_data = f.readlines.each {|line| line.chomp!}
+  f.close
+
+  inventory = {}
+  product_data.each { |i| inventory[i.split(',')[0].to_i] = [i.split(',')[1], i.split(',')[2].to_f] }
+  inventory
+end
+
+inventory, choice = startup, ''
+puts inventory
+
+while choice != '8'
+  puts menu
+  print "\nPlease enter a choice:"
+  choice = gets.chomp
+  case choice
+    when '1'
+      view_products(inventory)
+    when '2'
+      add_product(inventory)
+    when '3'
+      remove_product(inventory)
+    when '4'
+      update_product(inventory)
+    when '5'
+      highest_priced(inventory)
+    when '6'
+      lowest_priced(inventory)
+    when '7'
+      product_sum(inventory)
+    when '8'
+    else
+      puts "\nPlease enter a valid choice.\n"
+  end
+end
