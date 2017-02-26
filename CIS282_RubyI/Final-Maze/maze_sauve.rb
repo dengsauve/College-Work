78fc9181be2f29ac8f5a3905b7c24c042f8271cb
Dennis <dengsauve@yahoo.com>
2017-02-26 05:12:51 -0800

    Completed exploration expansion, backtracking needs work

--- CIS282_RubyI/Final-Maze/maze_sauve.rb
+++ CIS282_RubyI/Final-Maze/maze_sauve.rb
@@ -24,12 +24,12 @@ end
 # 2.	Print out the maze before it is solved
 
 def print_maze(maze)
-  maze.each { |row| row.each { |col| print col};puts}
+  maze.each { |row| row.each { |col| print col.to_s.ljust(2)};puts}
 end
 
 # 3.	Solve the maze by printing a character "path" from the beginning to the end which is specified by S (start) and F (finish).
 
-def pathfinder(maze, counter=0, unexplored_territory=true)
+def pathfinder(maze, counter=0, unexplored_territory=true, starting_cell=[])
   while counter==0
     maze.each_with_index do |array, row|
       array.each_with_index do |cell, col|
@@ -52,11 +52,75 @@ def pathfinder(maze, counter=0, unexplored_territory=true)
           end
         end
       end
-      end
-      counter+=1
+    end
+    counter+=1
   end
   print_maze(maze) if $debug
-
+  while unexplored_territory
+    maze.each_with_index do |array, row|
+      array.each_with_index do |cell, col|
+        if cell == (counter-1)
+          #east
+          if maze[row][col+1] == 'S'
+            unexplored_territory = false
+            starting_cell = [row, col]
+          elsif maze[row][col+1] == ' '
+              maze[row][col+1] = counter
+          end
+          #north
+          if maze[row-1][col] == 'S'
+            unexplored_territory = false
+            starting_cell = [row, col]
+          elsif maze[row-1][col] == ' '
+              maze[row-1][col] = counter
+          end
+          #west
+          if maze[row][col-1] == 'S'
+            unexplored_territory = false
+            starting_cell = [row, col]
+          elsif maze[row][col-1] == ' '
+              maze[row][col-1] = counter
+          end
+          #south
+          if maze[row+1][col] == 'S'
+            unexplored_territory = false
+            starting_cell = [row, col]
+          elsif maze[row+1][col] == ' '
+              maze[row+1][col] = counter
+          end
+        end
+      end
+    end
+    counter+=1
+    if $debug
+      print_maze(maze)
+      gets
+    end
+  end
+  counter = counter - 3
+  print counter
+  while counter >= 0
+    maze[starting_cell[0]][starting_cell[1]] = '*'
+    if maze[starting_cell[0]][starting_cell[1]+1].is_a?(Integer)#east
+      if maze[starting_cell[0]][starting_cell[1]+1].to_i == counter
+        starting_cell = [starting_cell[0],starting_cell[1]+1]
+      end
+    elsif maze[starting_cell[0]-1][starting_cell[1]].is_a?(Integer)#north
+      if maze[starting_cell[0]-1][starting_cell[1]].to_i == counter
+        starting_cell = [starting_cell[0]-1,starting_cell[1]]
+      end
+    elsif maze[starting_cell[0]][starting_cell[1]-1].is_a?(Integer)#west
+      if maze[starting_cell[0]][starting_cell[1]-1].to_i == counter
+        starting_cell = [starting_cell[0],starting_cell[1]-1]
+      end
+    elsif maze[starting_cell[0]+1][starting_cell[1]].is_a?(Integer)#south
+      if maze[starting_cell[0]+1][starting_cell[1]].to_i == counter
+        starting_cell = [starting_cell[0]+1,starting_cell[1]]
+      end
+    end
+    counter -= 1
+  end
+  maze
 end
 
 
@@ -82,15 +146,8 @@ end
 maze = read_maze(0)
 # puts maze.inspect
 print_maze(maze)
-pathfinder(maze)
-
-
-
-
-
-
-
-
+maze = pathfinder(maze)
+print_maze(maze)
 
 
 
