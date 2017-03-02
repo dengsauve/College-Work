fb48f8df0a890e727057de93492f642aa59183a7
Dennis <dengsauve@yahoo.com>
2017-03-02 05:13:47 -0800

    Completed Coin Extra Credit

---
+++ CIS282_RubyI/Extra-Credit/2_reverse_digits_sauve.rb
@@ -0,0 +1,9 @@
+############################################################
+#
+#  Name:         Dennis Sauve
+#  Date:         03/02/17
+#  Assignment:   Reverse Digits Extra Credit
+#  Class:        CIS 282
+#  Description:  Coin Counter that sums the values of the coins given
+#
+############################################################
