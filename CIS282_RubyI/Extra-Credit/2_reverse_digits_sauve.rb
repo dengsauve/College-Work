d2300fba2f2b940bb461673ce0686b0f9f32d1f6
Dennis <dengsauve@yahoo.com>
2017-03-02 05:45:27 -0800

    Completed Reverse Digits and Acronym

--- CIS282_RubyI/Extra-Credit/2_reverse_digits_sauve.rb
+++ CIS282_RubyI/Extra-Credit/2_reverse_digits_sauve.rb
@@ -4,6 +4,7 @@
 #  Date:         03/02/17
 #  Assignment:   Reverse Digits Extra Credit
 #  Class:        CIS 282
-#  Description:  Coin Counter that sums the values of the coins given
+#  Description:  Recursively reverses numbers.
 #
 ############################################################
+def reverse_places(number) print number % 10; reverse_places(number/10) if number/10 != 0 end
--- CIS282_RubyI/Extra-Credit/3_acronym_sauve.rb
+++ CIS282_RubyI/Extra-Credit/3_acronym_sauve.rb
@@ -4,6 +4,7 @@
 #  Date:         03/02/17
 #  Assignment:   Acronym Extra Credit
 #  Class:        CIS 282
-#  Description:  Coin Counter that sums the values of the coins given
+#  Description:  Returns uppercase first letter of every word
 #
 ############################################################
+def acronym(phrase, return_acronym='') phrase.split().each { |word| return_acronym += word[0].upcase}; return_acronym end
