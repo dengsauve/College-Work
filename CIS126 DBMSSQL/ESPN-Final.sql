22f66f542c3ce7df7cddbc980e96dac27e1a9199
Dennis <dengsauve@yahoo.com>
2017-03-13 12:15:39 -0700

    Calculated SLG and OPS

--- CIS126 DBMSSQL/ESPN-Final.sql
+++ CIS126 DBMSSQL/ESPN-Final.sql
@@ -6,13 +6,13 @@ select G as 'GP',
 	B3 as '3B',
 	HR,
 	RBI,
-	((S)+(B2*2)+(B3*3)+(HR*4)) as TB,
+	((S)+(B2*2)+(B3*3)+(HR*4)) as 'TB',
 	BB,
 	SO,
 	SB,
-	(H*1.0)/(case when AB=0 then 1 else AB end) as BA,
-	((H + BB + HBP)*1.0 / (case when (AB + BB + HBP)=0 then 1 else (AB + BB + HBP) end)) as OBP
-	/* Needs SLG (Slugging Percentage) */
-	/* Needs OPS (OBP + SLG) */
+	(H*1.0)/(case when AB=0 then 1 else AB end) as 'BA',
+	((H + BB + HBP)*1.0 / (case when (AB + BB + HBP)=0 then 1 else (AB + BB + HBP) end)) as 'OBP',
+	((S)+(B2*2)+(B3*3)+(HR*4))*1.0 / (case when AB=0 then 1 else AB end) as 'SLG',
+	((H + BB + HBP)*1.0 / (case when (AB + BB + HBP)=0 then 1 else (AB + BB + HBP) end)) + (((S)+(B2*2)+(B3*3)+(HR*4))*1.0 / (case when AB=0 then 1 else AB end)) as 'OPS'
 	from db_Baseball2015.vwPlayersBatting
 	where yearID=2010
