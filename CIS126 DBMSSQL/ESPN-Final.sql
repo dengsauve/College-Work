a6a4aa0abc49624254f2a5345f48396b6c799654
Dennis <dengsauve@yahoo.com>
2017-03-13 13:19:15 -0700

    Coded in the Footer of the Table

--- CIS126 DBMSSQL/ESPN-Final.sql
+++ CIS126 DBMSSQL/ESPN-Final.sql
@@ -17,3 +17,26 @@ select nameFirst + ' ' + nameLast as 'NAME',
 	((H + BB + HBP)*1.0 / (case when (AB + BB + HBP)=0 then 1 else (AB + BB + HBP) end)) + (((S)+(B2*2)+(B3*3)+(HR*4))*1.0 / (case when AB=0 then 1 else AB end)) as 'OPS'
 	from db_Baseball2015.vwPlayersBatting
 	where yearID=2010 and teamID='LAN'
+	order by 'BA' desc;
+
+select teamID as 'NAME',
+	MAX(G) as 'GP',
+	SUM(AB) as 'AB',
+	SUM(R) as 'R',
+	SUM(H) as 'H',
+	SUM(B2) as '2B',
+	SUM(B3) as '3B',
+	SUM(HR) as 'HR',
+	SUM(RBI) as 'RBI',
+	(SUM(S)+(SUM(B2)*2)+(SUM(B3)*3)+(SUM(HR)*4)) as 'TB',
+	SUM(BB) as 'BB',
+	SUM(SO) as 'SO',
+	SUM(SB) as 'SB',
+	(SUM(H)*1.0)/(case when SUM(AB)=0 then 1 else SUM(AB) end) as 'BA',
+	((SUM(H) + SUM(BB) + SUM(HBP))*1.0 / (case when (SUM(AB) + SUM(BB) + SUM(HBP))=0 then 1 else (SUM(AB) + SUM(BB) + SUM(HBP)) end)) as 'OBP',
+	((SUM(S))+(SUM(B2)*2)+(SUM(B3)*3)+(SUM(HR)*4))*1.0 / (case when SUM(AB)=0 then 1 else SUM(AB) end) as 'SLG',
+	((SUM(H) + SUM(BB) + SUM(HBP))*1.0 / (case when (SUM(AB) + SUM(BB) + SUM(HBP))=0 then 1 else (SUM(AB) + SUM(BB) + SUM(HBP)) end)) + 
+		(((SUM(S))+(SUM(B2)*2)+(SUM(B3)*3)+(SUM(HR)*4))*1.0 / (case when SUM(AB)=0 then 1 else SUM(AB) end)) as 'OPS'
+	from db_Baseball2015.vwPlayersBatting
+	where yearID=2010 and teamID='LAN'
+	group by teamID
