54e1e7b04173700ca6414ba9c35302ffd4b89efb
Dennis Sauve <dengsauve@yahoo.com>
2017-04-11 12:08:07 -0700

    added prompt method

--- 2017 Spring/CIS283/sauve_enrollment.rb
+++ 2017 Spring/CIS283/sauve_enrollment.rb
@@ -10,36 +10,39 @@
 #
 ############################################################
 
-# setup endless loop for looking for classes
-department = ''
+def prompt(string) # takes a string prompt, returns answer
+  print string
+  gets.chomp.upcase
+end
+
+# get input from user (checks for exit at while loop)
+department = prompt('Enter Department or EXIT: ')
+
 # read the text file of classes
 enrollment = File.open('Enrollment.txt', 'rb').read
 
-while department != 'EXIT'
+while department != 'EXIT' # setup ~endless loop for looking for classes
 
   # get input from user
-  print 'Enter Department: '
-  department = gets.chomp.upcase
-  break if department == 'EXIT'
-  print 'Enter Class Number: '
-  class_number = gets.chomp
+  class_number = prompt('Enter Class Number: ')
 
   # boolean for printing sub-lines
   class_active = false
 
-  enrollment.split("\n").each do |line|
-
+  enrollment.split("\n").each do |line| #split for line-by-line search
     # checking to see if the line is a sub-line, and if it's looking for a sub-line for a class
     if line[5] == ' ' and class_active == true
       puts line
     else
       class_active = false # gets reset whenever there's a new value in line[5] other than ' '
     end
-
     # checks for department abbr. + space(s) and the class number
     if line =~ /#{department}\s+#{class_number}/
       puts line
       class_active = true # telling the first if/else to look for a sub-line
     end
   end
+
+  # get input from user, placed at bottom to get rid of IF statement/break
+  department = prompt('Enter Department or EXIT: ')
 end
