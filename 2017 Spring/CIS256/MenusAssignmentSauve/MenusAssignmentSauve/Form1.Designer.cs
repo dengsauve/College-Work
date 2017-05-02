9425c419ae69dd60769aac667562fb08913dbbcd
Dennis <dengsauve@yahoo.com>
2017-05-01 19:08:35 -0700

    Completed Initial Menus Assignment
    Time to check for bugs

---
+++ 2017 Spring/CIS256/MenusAssignmentSauve/MenusAssignmentSauve/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace MenusAssignmentSauve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemCalculate = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtTheater = new System.Windows.Forms.TextBox();
+            this.lblTheater = new System.Windows.Forms.Label();
+            this.lblTickets = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkIsStudent = new System.Windows.Forms.CheckBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.numTickets = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLabelTotal = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTickets)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemFile,
+            this.menuItemEdit,
+            this.menuItemCalculate});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(226, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuMain";
+            // 
+            // menuItemFile
+            // 
+            this.menuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemExit});
+            this.menuItemFile.Name = "menuItemFile";
+            this.menuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.menuItemFile.Text = "&File";
+            // 
+            // menuItemEdit
+            // 
+            this.menuItemEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemClear});
+            this.menuItemEdit.Name = "menuItemEdit";
+            this.menuItemEdit.Size = new System.Drawing.Size(39, 20);
+            this.menuItemEdit.Text = "&Edit";
+            // 
+            // menuItemClear
+            // 
+            this.menuItemClear.Name = "menuItemClear";
+            this.menuItemClear.Size = new System.Drawing.Size(152, 22);
+            this.menuItemClear.Text = "Clear";
+            this.menuItemClear.Click += new System.EventHandler(this.menuItemClear_Click);
+            // 
+            // menuItemExit
+            // 
+            this.menuItemExit.Name = "menuItemExit";
+            this.menuItemExit.Size = new System.Drawing.Size(152, 22);
+            this.menuItemExit.Text = "&Exit";
+            this.menuItemExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // menuItemCalculate
+            // 
+            this.menuItemCalculate.Name = "menuItemCalculate";
+            this.menuItemCalculate.Size = new System.Drawing.Size(68, 20);
+            this.menuItemCalculate.Text = "&Calculate";
+            this.menuItemCalculate.Click += new System.EventHandler(this.menuItemCalculate_Click);
+            // 
+            // txtTheater
+            // 
+            this.txtTheater.Location = new System.Drawing.Point(137, 27);
+            this.txtTheater.Name = "txtTheater";
+            this.txtTheater.Size = new System.Drawing.Size(74, 20);
+            this.txtTheater.TabIndex = 0;
+            // 
+            // lblTheater
+            // 
+            this.lblTheater.AutoSize = true;
+            this.lblTheater.Location = new System.Drawing.Point(12, 30);
+            this.lblTheater.Name = "lblTheater";
+            this.lblTheater.Size = new System.Drawing.Size(119, 13);
+            this.lblTheater.TabIndex = 4;
+            this.lblTheater.Text = "Movie Theater Number:";
+            // 
+            // lblTickets
+            // 
+            this.lblTickets.AutoSize = true;
+            this.lblTickets.Location = new System.Drawing.Point(12, 56);
+            this.lblTickets.Name = "lblTickets";
+            this.lblTickets.Size = new System.Drawing.Size(97, 13);
+            this.lblTickets.TabIndex = 5;
+            this.lblTickets.Text = "Number of Tickets:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Are you a Student:";
+            // 
+            // chkIsStudent
+            // 
+            this.chkIsStudent.AutoSize = true;
+            this.chkIsStudent.Location = new System.Drawing.Point(137, 80);
+            this.chkIsStudent.Name = "chkIsStudent";
+            this.chkIsStudent.Size = new System.Drawing.Size(44, 17);
+            this.chkIsStudent.TabIndex = 2;
+            this.chkIsStudent.Text = "Yes";
+            this.chkIsStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(137, 104);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(74, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // numTickets
+            // 
+            this.numTickets.Location = new System.Drawing.Point(137, 54);
+            this.numTickets.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numTickets.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numTickets.Name = "numTickets";
+            this.numTickets.Size = new System.Drawing.Size(74, 20);
+            this.numTickets.TabIndex = 1;
+            this.numTickets.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.lblLabelTotal);
+            this.groupBox1.Location = new System.Drawing.Point(17, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 41);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblLabelTotal
+            // 
+            this.lblLabelTotal.AutoSize = true;
+            this.lblLabelTotal.Location = new System.Drawing.Point(6, 16);
+            this.lblLabelTotal.Name = "lblLabelTotal";
+            this.lblLabelTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblLabelTotal.TabIndex = 0;
+            this.lblLabelTotal.Text = "Total:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(46, 16);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(142, 13);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "$";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 70);
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.calculateToolStripMenuItem.Text = "Calculate";
+            this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 189);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numTickets);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.chkIsStudent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTickets);
+            this.Controls.Add(this.lblTheater);
+            this.Controls.Add(this.txtTheater);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Movie Tickets";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTickets)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem menuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem menuItemEdit;
+        private System.Windows.Forms.ToolStripMenuItem menuItemClear;
+        private System.Windows.Forms.ToolStripMenuItem menuItemCalculate;
+        private System.Windows.Forms.TextBox txtTheater;
+        private System.Windows.Forms.Label lblTheater;
+        private System.Windows.Forms.Label lblTickets;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkIsStudent;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.NumericUpDown numTickets;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblLabelTotal;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ 2017 Spring/CIS256/MenusAssignmentSauve/MenusAssignmentSauve/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenusAssignmentSauve
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double baseCost = 7.50;
+            baseCost = baseCost * (int)numTickets.Value;
+
+            if (txtTheater.Text == "2")
+            {
+                baseCost = baseCost * 0.75;
+            }
+            else if(txtTheater.Text == "3")
+            {
+                baseCost += 5.0;
+            }
+
+            if (chkIsStudent.Checked)
+            {
+                baseCost -= 2.0;
+            }
+
+            baseCost = Math.Round(baseCost, 1);
+
+            lblTotal.Text = "$" + baseCost.ToString() + "0";
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuItemClear_Click(object sender, EventArgs e)
+        {
+            txtTheater.Text = "";
+            numTickets.Value = 1;
+            chkIsStudent.Checked = false;
+            lblTotal.Text = "$";
+            txtTheater.Focus();
+        }
+
+        private void menuItemCalculate_Click(object sender, EventArgs e)
+        {
+            btnCalculate.PerformClick();
+        }
+
+        private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnCalculate.PerformClick();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuItemClear.PerformClick();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuItemExit.PerformClick();
+        }
+    }
+}
