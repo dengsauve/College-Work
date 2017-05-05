52b4172eba57fd60747db8dc42210c3507973871
Dennis <dengsauve@yahoo.com>
2017-05-05 04:38:34 -0700

    Ruby II - Menu Class
    Followed Dave's YouTube lecture on the menu class. This will be a large help.

---
+++ 2017 Spring/CIS283/menu.rb
@@ -0,0 +1,34 @@
+############################################################
+#
+#  Name:         Dennis Sauve
+#  Date:         05/05/2017
+#  Class:        CIS283
+#  Description:  Menu Class - Accepts * string values as
+#                menu options. Quit must be last.
+#  Usage:        menu = Menu.new( <1 or more string options> )
+#                puts menu.get_menu_choice
+#                <output: user_choice as int>
+#                puts menu.quit
+#                <output: quit option in menu as int>
+############################################################
+
+class Menu
+  # Menu Choices
+  attr_reader :quit
+  def initialize(*menu_args) # * is called 'Splat', indicates unknown number of args
+    @menu_args = menu_args
+    @quit = @menu_args.length
+  end
+
+  def get_menu_choice
+    # print menu
+    @menu_args.each_with_index do |item, index|
+      puts "#{index + 1}. #{item}"
+    end
+    # ask for user choice
+    print 'Please enter your choice: '
+    user_choice = gets.to_i
+    # return user choice
+    return user_choice
+  end
+end
