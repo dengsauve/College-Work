bdb72f674199d8195542d85efb18e385a1e6a921
Dennis <dengsauve@yahoo.com>
2017-05-10 14:24:12 -0700

    Overload MiniAssignment

---
+++ 2017 Spring/CIS256/sauveMiniValueReturningMethods/sauveMiniValueReturningMethods/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace sauveMiniValueReturningMethods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSalesTax = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Price:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(115, 57);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(138, 20);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(115, 102);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(138, 20);
+            this.txtQuantity.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Quantity:";
+            // 
+            // txtSalesTax
+            // 
+            this.txtSalesTax.Location = new System.Drawing.Point(115, 149);
+            this.txtSalesTax.Name = "txtSalesTax";
+            this.txtSalesTax.Size = new System.Drawing.Size(138, 20);
+            this.txtSalesTax.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sales Tax:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculatus Eliminatus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(115, 233);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(138, 20);
+            this.txtAnswer.TabIndex = 7;
+            this.txtAnswer.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 302);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSalesTax);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSalesTax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAnswer;
+    }
+}
+
---
+++ 2017 Spring/CIS256/sauveMiniValueReturningMethods/sauveMiniValueReturningMethods/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sauveMiniValueReturningMethods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrWhiteSpace(txtQuantity.Text) && String.IsNullOrWhiteSpace(txtSalesTax.Text))
+            {
+                txtAnswer.Text = GetPrice(double.Parse(txtPrice.Text)).ToString();
+            }else if (String.IsNullOrWhiteSpace(txtSalesTax.Text))
+            {
+                txtAnswer.Text = GetPrice(double.Parse(txtPrice.Text), double.Parse(txtQuantity.Text)).ToString();
+            }else
+            {
+                txtAnswer.Text = GetPrice(double.Parse(txtPrice.Text), double.Parse(txtQuantity.Text), double.Parse(txtSalesTax.Text)).ToString();
+            }
+        }
+
+        private double GetPrice(double price)
+        {
+            return price;
+        }
+
+        private double GetPrice(double price, double quantity)
+        {
+            return price * quantity;
+        }
+
+        private double GetPrice(double price, double quantity, double salesTax)
+        {
+            return price * quantity * (1 + salesTax);
+        }
+    }
+}
