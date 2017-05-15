a6174aab89cd810687894ba145e90206cd2fa48e
Dennis <dengsauve@yahoo.com>
2017-05-15 08:35:48 -0700

    Midterm Problems 2 and 3

--- 2017 Spring/CIS256/MethodsProjectMidterm/MethodsProjectMidterm/Form1.cs
+++ 2017 Spring/CIS256/MethodsProjectMidterm/MethodsProjectMidterm/Form1.cs
@@ -24,12 +24,24 @@ namespace MethodsProjectMidterm
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            calculate();
+            //calculate();
+            //oldCalculate(double.Parse(txtNum1.Text), double.Parse(TextNum2.Text));
+            lblProduct.Text = calculate(double.Parse(txtNum1.Text), double.Parse(TextNum2.Text)).ToString();
         }
 
         public void calculate()
         {
             lblProduct.Text = (double.Parse(txtNum1.Text) * double.Parse(TextNum2.Text)).ToString();
+        }
+
+        public void oldCalculate(double num1, double num2)
+        {
+            lblProduct.Text = (num1 * num2).ToString();
+        }
+
+        public double calculate(double num1, double num2)
+        {
+            return num1 * num2;
         }
     }
 }
--- 2017 Spring/CIS256/MethodsProjectMidtermAnswers.cs
+++ 2017 Spring/CIS256/MethodsProjectMidtermAnswers.cs
@@ -1,8 +1,17 @@
 // Problem 1
 public void calculate()
-	{
-		lblProduct.Text = (double.Parse(txtNum1.Text) * double.Parse(TextNum2.Text)).ToString();
-	}
+		{
+			lblProduct.Text = (double.Parse(txtNum1.Text) * double.Parse(TextNum2.Text)).ToString();
+		}
 	
 // Problem 2
-
+public void calculate(double num1, double num2)
+        {
+            lblProduct.Text = (num1 * num2).ToString();
+        }
+		
+// Problem 3
+public double calculate(double num1, double num2)
+        {
+            return num1 * num2;
+        }
