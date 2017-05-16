a134da6c2d69cd9eb2968deafaac532fd4897ee9
Dennis Sauve <dengsauve@yahoo.com>
2017-05-16 13:34:29 -0700

    Followed along with Dave
    Tuesday Lecture

--- 2017 Spring/CIS283/network.rb
+++ 2017 Spring/CIS283/network.rb
@@ -1,3 +1,20 @@
+require 'net/https'
+require 'prawn'
+
 website = ARGV[0]
 puts website
+puts ARGV.inspect
+response = Net::HTTP.get_response( website, '/')
+puts response.code
+puts response.body
+
+Prawn::Document.generate( "pdf_bin/helloworld.pdf" ) do
+  font "Courier", :size => 24
+  text "Hello World", :align => :center
+  font "Courier", :size => 10
+  text "#{website} - #{response.code}", :align => :left
+  start_new_page
+  #font "Courier", :size => 10
+  #text "#{response.body}"
+end
 
