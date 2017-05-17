7e2922d5b300d4bfcb5c23848bfd21f225ceaa3f
Dennis <dengsauve@yahoo.com>
2017-05-17 13:29:27 -0700

    POD Assignment

---
+++ 2017 Spring/CIS256/ClassAssignmentPOD/ClassAssignmentPOD/PurchaseOrder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignmentPOD
+{
+    public class PurchaseOrder
+    {
+        private string oCompanyName;
+        private string oPartNumber;
+        private int oQtyPurchased;
+        private double oUnitPrice;
+        private bool oDiscount;
+
+        public PurchaseOrder()
+        {
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                return oCompanyName;
+            }
+
+            set
+            {
+                oCompanyName = value;
+            }
+        }
+
+        public string PartNumber
+        {
+            get
+            {
+                return oPartNumber;
+            }
+            set
+            {
+                oPartNumber = value;
+            }
+        }
+
+        public int QtyPurchased
+        {
+            get
+            {
+                return oQtyPurchased;
+            }
+            set
+            {
+                oQtyPurchased = value;
+            }
+        }
+
+        public double UnitPrice
+        {
+            get
+            {
+                return oUnitPrice;
+            }
+            set
+            {
+                oUnitPrice = value;
+            }
+        }
+
+        public bool Discount
+        {
+            get
+            {
+                return oDiscount;
+            }
+            set
+            {
+                oDiscount = value;
+            }
+        }
+    }
+}
