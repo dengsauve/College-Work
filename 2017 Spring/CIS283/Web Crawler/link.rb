a35f994d27c50326ca4c30f45853809fc69e2ada
Dennis <dengsauve@yahoo.com>
2017-05-20 11:13:19 -0700

    Progress Towards Report Writing
    Issues with Multi-Lingual Text

--- 2017 Spring/CIS283/Web Crawler/link.rb
+++ 2017 Spring/CIS283/Web Crawler/link.rb
@@ -9,9 +9,11 @@
 #
 ############################################################
 
+
 class Link_Checker
 
   attr_reader :page_body
+  attr_accessor :link_list
 
   def initialize(website_url, page_body)
     @@base_url = website_url
@@ -19,31 +21,36 @@ class Link_Checker
     @link_list = []
   end
 
-  def find_links
-    puts "find links reached"
-    @page_body.scan(/<a.*href=["'](?:http:\/\/)?([^'"]*)(?:\/)?["'].*>(.*)<\/a>/).each do | pair |
-      puts "scanning... #{pair[0]}"
-      if pair[0] =~ /#{@@base_url}/
-        @link_list << Link.new(pair[0], pair[1])
-      elsif pair[0] =~ /^(?:\/.*)+/
-        @link_list << Link.new(@@base_url + pair[0], pair[1])
-      elsif pair[0] =~ /\#.*/
-        nil
-      else
-        @link_list << Link.new(pair[0], pair[1])
-      end
-    end
+  def add_link(link)
+    @link_list << link
   end
 
   def base_url
     return @@base_url
   end
 
+  def print_links
+    ret_str = ''
+    @link_list.each do | link |
+      ret_str += link.to_s + "\n" unless link.code == '404'
+    end
+    return ret_str
+  end
+
+  def print_bad_links
+    ret_str = ''
+    @link_list.each do | link |
+      ret_str += link.to_s + "\n" if link.code == '404'
+    end
+    return ret_str
+  end
+
 end
 
 class Link < Link_Checker
+  attr_reader :code
   def initialize(link, click_value)
-    @link = link
+    @link = link[-1] == '/' ? link[0...-1] : link
     @click_value = click_value
     @link_type = check_base
     @code = check_link
@@ -54,15 +61,21 @@ class Link < Link_Checker
   end
 
   def to_s
-    return "#{@link} | #{@click_value}"
+    return @link.ljust(60, '.') + @click_value.rjust(30, '.') +  "\n" +
+        @link_type.ljust(60, '.') + @code.rjust(30, '.') + "\n"
   end
 
   def check_link
-    return Net::HTTP.get_response(@link, '/').code
+    if @link_type == 'internal'
+      return Net::HTTP.get_response(@@base_url, @link).code
+    else
+      @link.scan(/(?:https?:\/\/)?((?:www\.)?.+?\.[a-z]{2,6})(.*)/)
+      return Net::HTTP.get_response($1, $2).code
+    end
   end
 
   def check_base
-    if @link =~ /#{@@base_url}/
+    if @link =~ /^(?:\/.*)/
       return 'internal'
     else
       return 'external'
