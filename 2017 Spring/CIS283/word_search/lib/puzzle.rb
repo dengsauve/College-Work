baa6a83f58a5fbd499ddc789054996004ba4e1b0
Dennis <dengsauve@yahoo.com>
2017-06-10 23:04:23 -0700

    Solved issue of non-pointer copy of array

--- 2017 Spring/CIS283/word_search/lib/puzzle.rb
+++ 2017 Spring/CIS283/word_search/lib/puzzle.rb
@@ -12,7 +12,19 @@ class Puzzle
   attr_accessor :main_grid
   attr_reader :status_grid
   def initialize
-    @main_grid = Array.new(45, Array.new(45, ''))
+    @main_grid = Array.new(45) { Array.new(45, '.') }
     @status_grid = Array.new(45, Array.new(45, :open))
+  end
+
+  def to_s
+    ret_str = ''
+    @main_grid.each_with_index do | row, index |
+      ret_str += (index.to_s + '.').ljust(4) + row.join('') + "\n"
+    end
+    return ret_str
+  end
+
+  def get_grid
+    return Marshal.load(Marshal.dump(@main_grid))
   end
 end
--- 2017 Spring/CIS283/word_search/puzzle_main.rb
+++ 2017 Spring/CIS283/word_search/puzzle_main.rb
@@ -9,33 +9,57 @@
 ############################################################
 require_relative('lib/puzzle')
 
+def check(x, y)
+  return x >= 0 && x < 45 && y >= 0 && y < 45
+end
+
+# Create a method for getting a new start position.
+def get_start
+  return [Random.rand(45), Random.rand(45)]
+end
+
+# Method for getting a random direction
+def get_direction # most guys hate asking for directions...
+  ret_array = [0, 0]
+  while ret_array == [0, 0]
+    ret_array = [Random.rand(-1..1), Random.rand(-1..1)]
+  end
+  return ret_array
+end
+
+# Method to try and place word
+def try_place(word, word_search)
+  y, x = get_start
+  lambda_y, lambda_x = get_direction
+  puzzle, start_locations = word_search.get_grid, word_search.status_grid
+  word.split('').each do |letter|
+    puts letter
+    if check(x, y) && (puzzle[y][x] == '.' || puzzle[y][x] == letter)
+      puzzle[y][x] = letter
+      y, x = y + lambda_y, x + lambda_x
+    else
+      return false
+    end
+  end
+  word_search.main_grid=(puzzle)
+  return true
+end
+
 # Read the list of words and then sort them by the length of each word from largest to smallest
 word_list = File.read('lib/words.txt').split("\r\n").sort_by { |a| a.length }.reverse
-puts word_list.inspect
+# puts word_list.inspect
 word = word_list[0]
 
 # Select a random row/col to start your placement of the word.
 word_search = Puzzle.new
-start = [Random.rand(45), Random.rand(45)]
 
 # Choose a random “direction” to begin placement.
 # Start test-placing letters into the cells in the correct direction.
-data = [word, word_search, start]
-case Random.rand(8)
-  when 0
-    try_east(data)
-  when 1
-    north_east(data)
-  when 2
-    north(data)
-  when 3
-    north_west(data)
-  when 4
-    west(data)
-  when 5
-    south_west(data)
-  when 6
-    south(data)
-  when 7
-    south_east(data)
+puts word_search.to_s
+
+success = false
+until success
+  success = try_place(word, word_search)
 end
+
+puts word_search.to_s
