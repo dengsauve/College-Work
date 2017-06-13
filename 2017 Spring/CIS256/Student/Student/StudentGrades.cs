a052454ac1296c6c2d389ccb40808f3ebe559e2d
Dennis <dengsauve@yahoo.com>
2017-06-13 01:57:19 -0700

    Working on C# Final Project

---
+++ 2017 Spring/CIS256/Student/Student/StudentGrades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class StudentGrades
+    {
+        private string oFirstName;
+        private string oMiddleName;
+        private string oLastName;
+
+        public StudentGrades()
+        {
+
+        }
+
+        public StudentGrades(string firstName, string lastName)
+        {
+            oFirstName = firstName;
+            oLastName = lastName;
+        }
+
+        public StudentGrades(string firstName, string lastName, string middleName)
+        {
+            oFirstName = firstName;
+            oLastName = lastName;
+            oMiddleName = middleName;
+        }
+    }
+}
