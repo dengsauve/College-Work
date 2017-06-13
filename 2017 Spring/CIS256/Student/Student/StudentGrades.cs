d7711a445002760bca3fe1de36ecafafa8f936a9
Dennis <dengsauve@yahoo.com>
2017-06-13 02:41:34 -0700

    Added in getAddress function

--- 2017 Spring/CIS256/Student/Student/StudentGrades.cs
+++ 2017 Spring/CIS256/Student/Student/StudentGrades.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Student
 {
@@ -18,9 +19,9 @@ namespace Student
         private string oZip;
         private string oMajor;
         private DateTime oEnrolledDate;
-        private List<string> oCourseCompleted;
-        private List<string> oCourseQtrYr;
-        private List<double> oCourseGrade;
+        private List<string> oCourseCompleted = new List<string>();
+        private List<string> oCourseQtrYr = new List<string>();
+        private List<double> oCourseGrade = new List<double>();
 
         public StudentGrades()
         {
@@ -38,6 +39,90 @@ namespace Student
             oFirstName = firstName;
             oLastName = lastName;
             oMiddleName = middleName;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return oId;
+            }
+            set
+            {
+                oId = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return oEmail;
+            }
+            set
+            {
+                oEmail = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return oPhone;
+            }
+            set
+            {
+                oPhone = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return oAddress;
+            }
+            set
+            {
+                oAddress = value;
+            }
+        }
+
+        public string Zip
+        {
+            get
+            {
+                return oZip;
+            }
+            set
+            {
+                oZip = value;
+            }
+        }
+
+        public string Major
+        {
+            get
+            {
+                return oMajor;
+            }
+            set
+            {
+                oMajor = value;
+            }
+        }
+
+        public DateTime EnrolledDate
+        {
+            get
+            {
+                return oEnrolledDate;
+            }
+            set
+            {
+                oEnrolledDate = value;
+            }
         }
 
         public string getName()
@@ -61,5 +146,65 @@ namespace Student
         {
             return oFirstName + " " + oMiddleName + " " + oLastName;
         }
+
+        public string getAddress()
+        {
+            string city;
+            string state;
+            string name;
+            if (oMiddleName == "")
+            {
+                name = oFirstName + " " + oLastName;
+            }
+            else
+            {
+                name = oFirstName + " " + oMiddleName + " " + oLastName;
+            }
+
+            // Initialize the Strings
+            String cnStr, strSQL;
+
+            SqlConnection cn = new SqlConnection();
+            // NOTE: Could be SqlConnection cn = new SqlConnection("Data Source=134.39.173.35;Initial Catalog=Contributions;User ID=dsauve_w17;Password=HJpo11er");
+            // This has the advantage of being short and sweet.
+
+            cnStr = "Data Source=134.39.173.35;Initial Catalog=Contributions;User ID=dsauve_w17;Password=HJpo11er";
+            // NOTE: Could also be cn.ConnectionString = "Data Source=134.39.173.35;Initial Catalog=Contributions;User ID=dsauve_w17;Password=HJpo11er";
+            cn.ConnectionString = cnStr;
+
+            // Open the connetion to the SQL Server
+            cn.Open();
+
+            // Create the SQL Query
+            strSQL = "SELECT city, state FROM dbo.tblZipcodes where zip='" + oZip + "'";
+
+            // Create the SqlCommand Object
+            SqlCommand cmd = new SqlCommand(strSQL, cn);
+
+            // Create the SqlDataReader Object from Executing the SqlCommand Object
+            SqlDataReader rdrZip = cmd.ExecuteReader();
+
+            // Read the data from the SqlDataReader
+            rdrZip.Read();
+
+            // Check to see if there are any results
+            if (rdrZip.HasRows)
+            {
+                // Set the labels to the appropriate values
+                city = rdrZip["city"].ToString();
+                state = rdrZip["state"].ToString();
+            }
+            else
+            {
+                city = "";
+                state = "";
+            }
+            //Close the connection to SQl Server
+            cn.Close();
+
+            return name + "\n" + oAddress + "\n" + city + ", " + state + " " + oZip;
+        }
+
+
     }
 }
