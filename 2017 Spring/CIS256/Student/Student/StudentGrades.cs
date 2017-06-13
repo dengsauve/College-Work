579f42c80d530380f1b2d99a26376a243be8a7ab
Dennis <dengsauve@yahoo.com>
2017-06-13 02:30:06 -0700

    Progress being made

--- 2017 Spring/CIS256/Student/Student/StudentGrades.cs
+++ 2017 Spring/CIS256/Student/Student/StudentGrades.cs
@@ -11,10 +11,20 @@ namespace Student
         private string oFirstName;
         private string oMiddleName;
         private string oLastName;
+        private string oId;
+        private string oEmail;
+        private string oPhone;
+        private string oAddress;
+        private string oZip;
+        private string oMajor;
+        private DateTime oEnrolledDate;
+        private List<string> oCourseCompleted;
+        private List<string> oCourseQtrYr;
+        private List<double> oCourseGrade;
 
         public StudentGrades()
         {
-
+            // Gotcha Constructor
         }
 
         public StudentGrades(string firstName, string lastName)
@@ -28,6 +38,28 @@ namespace Student
             oFirstName = firstName;
             oLastName = lastName;
             oMiddleName = middleName;
+        }
+
+        public string getName()
+        {
+            if(oMiddleName == "")
+            {
+                return oFirstName + " " + oLastName;
+            }
+            else
+            {
+                return oFirstName + " " + oMiddleName + " " + oLastName;
+            }
+        }
+
+        public string getName(string firstName, string lastName)
+        {
+            return oFirstName + " " + oLastName;
+        }
+
+        public string getName(string firstName, string lastName, string middleName)
+        {
+            return oFirstName + " " + oMiddleName + " " + oLastName;
         }
     }
 }
