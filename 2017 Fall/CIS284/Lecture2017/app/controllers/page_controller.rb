2f6035c153483d6620ddd09bd5fc279e8efde14d
Dennis G. Sauve <dengsauve@yahoo.com>
2017-09-27 22:09:03 -0700

    9-27-17 Rails Lecture

---
+++ 2017 Fall/CIS284/Lecture2017/app/controllers/page_controller.rb
@@ -0,0 +1,22 @@
+class PageController < ApplicationController
+  def contact_us
+  end
+
+  def products
+  end
+
+  def preferences
+  end
+
+  def blog
+  end
+
+  def calendar
+  end
+
+  def articles
+  end
+
+  def login
+  end
+end
--- 2017 Fall/CIS284/Lecture2017/config/routes.rb
+++ 2017 Fall/CIS284/Lecture2017/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'page/contact_us'
+
+  get 'page/products'
+
+  get 'page/preferences'
+
+  get 'page/blog'
+
+  get 'page/calendar'
+
+  get 'page/articles'
+
+  get 'page/login'
+
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
