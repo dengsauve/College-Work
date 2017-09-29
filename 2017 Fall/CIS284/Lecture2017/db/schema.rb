ce33267f28ebf963b5a3e7a207dea89397e5d5a5
Dennis G. Sauve <dengsauve@yahoo.com>
2017-09-29 03:00:22 -0700

    Getting ready to develop on laptop

--- 2017 Fall/CIS284/Lecture2017/db/schema.rb
+++ 2017 Fall/CIS284/Lecture2017/db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 20170925235902) do
 
   create_table "movies", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "length"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.text "description"
+    t.integer "length"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
