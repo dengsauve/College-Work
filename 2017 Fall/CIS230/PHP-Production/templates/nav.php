f116bc1236c183c7b69466ecb488d4d2195de631
Dennis G. Sauve <dengsauve@yahoo.com>
2017-10-02 21:45:09 -0700

    Updated Navigation
    Updated to include active page highlighted programmatically.

--- 2017 Fall/CIS230/PHP-Production/templates/nav.php
+++ 2017 Fall/CIS230/PHP-Production/templates/nav.php
@@ -21,14 +21,14 @@
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li><a href="/index.php">Home</a></li> <!-- class="active" -->
-        <li><a href="/contactus.php">Contact Us</a></li>
-        <li><a href="/products.php">Products</a></li>
-        <li><a href="/blog.php">Blog</a></li>
-        <li><a href="/calendar.php">Calendar</a></li>
-        <li><a href="/maps.php">Maps</a></li>
-        <li><a href="/preferences.php">Preferences</a></li>
-        <li><a href="/login.php">Login</a></li>
+        <li <?php echo ($title == "Home" ? "class=\"active\"" : ""); ?>><a href="/index.php">Home</a></li> <!-- class="active" -->
+        <li <?php echo ($title == "Contact Us" ? "class=\"active\"" : ""); ?>><a href="/contactus.php">Contact Us</a></li>
+        <li <?php echo ($title == "Products" ? "class=\"active\"" : ""); ?>><a href="/products.php">Products</a></li>
+        <li <?php echo ($title == "Blog" ? "class=\"active\"" : ""); ?>><a href="/blog.php">Blog</a></li>
+        <li <?php echo ($title == "Calendar" ? "class=\"active\"" : ""); ?>><a href="/calendar.php">Calendar</a></li>
+        <li <?php echo ($title == "Maps" ? "class=\"active\"" : ""); ?>><a href="/maps.php">Maps</a></li>
+        <li <?php echo ($title == "Preferences" ? "class=\"active\"" : ""); ?>><a href="/preferences.php">Preferences</a></li>
+        <li <?php echo ($title == "Login" ? "class=\"active\"" : ""); ?>><a href="/login.php">Login</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
