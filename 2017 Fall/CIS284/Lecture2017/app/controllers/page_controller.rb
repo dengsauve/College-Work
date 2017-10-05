01ad8ba64cbae8f635926daab317b4a832ea2cde
Dennis G. Sauve <dengsauve@yahoo.com>
2017-10-04 22:11:54 -0700

    Ruby 10-4 Lecuture

--- 2017 Fall/CIS284/Lecture2017/app/controllers/page_controller.rb
+++ 2017 Fall/CIS284/Lecture2017/app/controllers/page_controller.rb
@@ -1,22 +1,41 @@
 class PageController < ApplicationController
   def contact_us
     @a = [1, 2, 3, 4, 5, 6]
+
     @now = DateTime.now
+
+    @first_name = params['first_name']
+    @last_name = params['last_name']
+
+    @error = ''
+    if @first_name.strip == ''
+      @error = "First Name is Required!"
+    end
+
+    @phone_number = params['phone_number']
+
+    @question = params['question']
+
+    @newsletter = params['newsletter']
+    @subscribed = @newsletter == 'subscribed' ? true : false
+
+    @gender = params['gender']
+
+    @restaurants = [
+        "Red Dragon Chinese",
+        "McDonalds",
+        "Taco Bell"
+    ]
+    @selected_restaurant = params['restaurants']
+
+  end
+
+  def products
     @products = {
         "Watermelon" => "Green",
         "Cantelope" => "Tan",
         "Lemon" => "Yellow"
     }
-    @first_name = params['first_name']
-    @last_name = params['last_name']
-    @phone_number = params['phone_number']
-    @question = params['question']
-    @newsletter = params['newsletter']
-    @subscribed = @newsletter == 'subscribed' ? true : false
-    @gender = params['gender']
-  end
-
-  def products
   end
 
   def preferences
