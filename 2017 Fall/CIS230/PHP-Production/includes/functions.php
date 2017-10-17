ba3920b23ec0a49c00a5e168ca8aee76e7f5f016
Dennis G. Sauve <dengsauve@yahoo.com>
2017-10-17 11:06:55 -0700

    Database Day!

--- 2017 Fall/CIS230/PHP-Production/includes/functions.php
+++ 2017 Fall/CIS230/PHP-Production/includes/functions.php
@@ -4,7 +4,12 @@
  * User: Dennis
  * Date: 10/4/2017
  * Time: 9:04 PM
+ *
+ * mysql -u dengsauve -p -h mysqldev.dennissauve.com phpdevdennissauvecom
+ *
+ * http://mysqldev.dennissauve.com/
  */
+
 
 /**
  * @param $selectBoxName
