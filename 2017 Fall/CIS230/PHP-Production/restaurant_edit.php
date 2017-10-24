269168827f0baca761abbe38e3addaf36031bc96
Dennis G. Sauve <dengsauve@yahoo.com>
2017-10-24 14:29:36 -0700

    Completed 10-24 PHP

--- 2017 Fall/CIS230/PHP-Production/restaurant_edit.php
+++ 2017 Fall/CIS230/PHP-Production/restaurant_edit.php
@@ -1,26 +1,64 @@
 <?php
 
+ob_start();
+
 $title = "Edit Restaurant";
 include 'includes/header.php';
 include 'includes/functions.php';
 
 $id = $_GET['id'];
-
+$submit = $_POST["submit"];
 $db = db_connect();
 
-$sql = "select * from restaurants where id=$id";
+if ( empty($submit) ) {
+  // Get data from the database
+  $sql = "select * from restaurants where id=$id";
 
-$result = $db->query($sql);
+  $result = $db->query($sql);
 
-mysqli_close( $db );
+  list($id, $name, $location, $priceRangeLow, $priceRangeHigh, $tags, $modifiedAt, $createdAt) = $result->fetch_row();
 
-list($id, $name, $location, $priceRangeLow, $priceRangeHigh, $tags, $modifiedAt, $createdAt) = $result->fetch_row();
+} else {
+  // Get data from posted form
+  $name = $_POST["name"];
+  $location = $_POST["location"];
+  $priceRangeLow = $_POST["priceRangeLow"];
+  $priceRangeHigh = $_POST["priceRangeHigh"];
+  $tags = $_POST["tags"];
+
+  // Check code for errors
+  // Update the database with POST data
+
+  $modifiedAt = date_create()->format("Y-m-d H:i:s");
+
+  $sql = "UPDATE restaurants SET name='$name',
+                                  location='$location',
+                                  priceRangeLow='$priceRangeLow',
+                                  priceRangeHigh='$priceRangeHigh',
+                                  tags='$tags',
+                                  modifiedAt='$modifiedAt'
+WHERE id='$id'";
+
+  echo $sql;
+
+  $result = $db->query($sql);
+
+  echo 'Updated Database!';
+
+  ob_clean();
+  header("Location: /restaurant.php?id=$id");
+  exit;
+}
+
+// Variables are populated
+
+mysqli_close($db);
 
 $form = <<<END_OF_EDITING
 
   <h1>Editing $name</h1>
   
-  <form method="POST" action="/restaurant_edit.php" class="text-left col-md-6 col-md-offset-3">
+  <form method="POST" action="/restaurant_edit.php?id=$id" class="text-left col-md-6 col-md-offset-3">
     
     <div class="form-group">
       <label for="name">Name:</label>
@@ -33,19 +71,26 @@ $form = <<<END_OF_EDITING
     </div>
     
     <div class="form-group">
-      <label for="priceLow">Price Range Low:</label>
-      <input type="number" class="form-control" name="priceLow" id="priceLow" value="$priceRangeLow" />
+      <label for="priceRangeLow">Price Range Low:</label>
+      <input type="number" class="form-control" name="priceRangeLow" id="priceRangeLow" value="$priceRangeLow" />
     </div>
     
     <div class="form-group">
-      <label for="priceHigh">Price Range High:</label>
-      <input class="form-control" type="number" name="priceHigh" id="priceHigh" value="$priceRangeHigh" />    
+      <label for="priceRangeHigh">Price Range High:</label>
+      <input class="form-control" type="number" name="priceRangeHigh" id="priceRangeHigh" value="$priceRangeHigh" />    
     </div>
     
     <div class="form-group">
       <label for="tags">Tags:</label>
       <input class="form-control" type="text" name="tags" id="tags" value="$tags" />    
-    </div>    
+    </div>
+    
+    <div class="form-group">
+      <input type="submit" name="submit" value="Save Changes" class="btn btn-primary" />
+      <a href="restaurant.php?id=$id" class="btn btn-default">Cancel</a>
+    </div>
+    
+    <a href="restaurants.php" class="btn btn-link btn-sm" role="button">Back to All Restaurants</a>
     
   </form>    
 
@@ -54,8 +99,8 @@ END_OF_EDITING;
 
 echo $form;
 
-
-
 include 'includes/footer.php';
+
+ob_end_flush();
 
 ?>
