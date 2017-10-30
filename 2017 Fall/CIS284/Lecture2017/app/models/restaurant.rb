5edd39493784732f6f7bc6fc1d0de463745597a3
Dennis G. Sauve <dengsauve@yahoo.com>
2017-10-30 11:30:21 -0700

    RAILS 10-23 RAILS

--- 2017 Fall/CIS284/Lecture2017/app/models/restaurant.rb
+++ 2017 Fall/CIS284/Lecture2017/app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+
+  # Ensures there's a name in the form
+  validates_presence_of :name
+
+  # Ensures that the price ranges are present and numbers and range
+  validates_numericality_of :low_price_range
+  validates_numericality_of :high_price_range, :greater_than => 1
+
+  # Using the newer method
+  validates :high_price_range, numericality: true, presence: true
+
 end
