f1c6e68887fc7e37541fda79f25bc29d9ca4d116
Dennis G. Sauve <dengsauve@yahoo.com>
2017-11-01 15:07:00 -0700

    Completed Product Model
    REST API and formatting completed. Turned in.

--- 2017 Fall/CIS284/Lecture2017/app/models/product.rb
+++ 2017 Fall/CIS284/Lecture2017/app/models/product.rb
@@ -7,10 +7,11 @@ class Product < ApplicationRecord
   validates :description, presence: true
 
   # Ensure qty on hand, ensure is_numeric, ensure no integer
-  validates :quantity_on_hand, numericality: true, presence: true
+  validates :quantity_on_hand, numericality: {greater_than_or_equal_to: 0}, presence: true
 
   # Ensure selling price, ensure is_numeric
-  validates :selling_price, numericality: true
+  # allow_blank in case a sale price hasn't been decided upon
+  validates :selling_price, numericality: {greater_than_or_equal_to: 0}, allow_blank: true
 
   # Ensure Cost, ensure is_numeric
   validates :cost, numericality: true, presence: true
@@ -18,12 +19,9 @@ class Product < ApplicationRecord
   # Ensure shipping weight, ensure is_numeric
   validates :shipping_weight, numericality: true
 
-  # Validates images URLS using 'valid_url' gem
-  validates :image_thumbnail, url: true, presence: false
-  validates :image_full, url: true, presence: false
-
-
-
-
+  # Validates images URLS using 'valid_url' gem.
+  # allow_blank in case there's no image to be had.
+  validates :image_thumbnail, :allow_blank => true, url: true
+  validates :image_full, :allow_blank => true, url: true
 
 end
