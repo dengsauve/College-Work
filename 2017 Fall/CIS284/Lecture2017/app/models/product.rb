1a148f9eaa157799bb64d871082dcee311934eff
Dennis G. Sauve <dengsauve@yahoo.com>
2017-11-01 12:31:29 -0700

    Finished Product Model in RAILS
    Needs to be given a once over to be sure

--- 2017 Fall/CIS284/Lecture2017/app/models/product.rb
+++ 2017 Fall/CIS284/Lecture2017/app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ApplicationRecord
+
+  # Ensure name
+  validates :name, presence: true
+
+  # Ensure description
+  validates :description, presence: true
+
+  # Ensure qty on hand, ensure is_numeric, ensure no integer
+  validates :quantity_on_hand, numericality: true, presence: true
+
+  # Ensure selling price, ensure is_numeric
+  validates :selling_price, numericality: true
+
+  # Ensure Cost, ensure is_numeric
+  validates :cost, numericality: true, presence: true
+
+  # Ensure shipping weight, ensure is_numeric
+  validates :shipping_weight, numericality: true
+
+  # Validates images URLS using 'valid_url' gem
+  validates :image_thumbnail, url: true, presence: false
+  validates :image_full, url: true, presence: false
+
+
+
+
+
 end
