a596299490331220b32e2ee0fc1d68246cf92e71
dengsauve <dengsauve@yahoo.com>
2017-11-29 21:46:13 -0800

    Completed Authentication Assignment

--- 2017 Fall/CIS284/Lecture2017/config/routes.rb
+++ 2017 Fall/CIS284/Lecture2017/config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   root 'page#articles'
 
   resources :movies
+
+  resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
