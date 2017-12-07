1af4f4645ed7a786ac9541068284e7f548c9c51e
Dennis G. Sauve <dengsauve@yahoo.com>
2017-12-06 23:15:45 -0800

    Created Products Index Page

--- 2017 Fall/CIS230/PHP-Production/products.php
+++ 2017 Fall/CIS230/PHP-Production/products.php
@@ -2,42 +2,64 @@
 
 $title = "Products";
 include 'includes/header.php';
+include 'includes/functions.php';
 
-?>
-
-  <h1>Products</h1>
-
-  <section class="well-lg col-xs-12">
-    <section class="col-xs-12 col-sm-6 text-left">
-      <h3>Watermelon Wedge <small>$1.00</small></h3>
-    </section>
-    <section class="col-xs-12 col-sm-6">
-      <img src="images/watermelon_wedge.jpg" class=product-image img-fluid" />
-    </section>
-  </section>
-
-  <section class="well-lg col-xs-12">
-    <section class="col-xs-12 col-sm-6 text-left">
-      <h3>Watermelon <small>$4.00</small></h3>
-    </section>
-    <section class="col-xs-12 col-sm-6">
-      <img src="images/watermelon.jpg" class="product-image img-fluid" />
-    </section>
-  </section>
-
-  <section class="well-lg col-xs-12">
-    <section class="col-xs-12 col-sm-6 text-left">
-      <h3>Watermelons <small>$400.00</small></h3>
-    </section>
-    <section class="col-xs-12 col-sm-6">
-      <img src="images/watermelons.jpg" class="product-image img-fluid" />
-    </section>
-  </section>
+//  Name, Description, Price, Cost, Qty On Hand, Image, Thumbnail Image
 
 
+$page_head = <<<END_OF_PAGE_HEAD
+
+<h1>Products</h1>
+<hr/>
+
+<div class="table-responsive">
+  <table class="table text-left">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Image</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th class="text-right">Price</th>
+        <th class="text-right">Cost</th>
+        <th class="text-right">Qty.</th>
+      </tr>
+    </thead>
+    <tbody>
+
+END_OF_PAGE_HEAD;
+
+echo $page_head;
 
 
-<?php
+$db = db_connect();
+$sql = "select * from products";
+$result = $db->query($sql);
+
+while( list($id, $name, $description, $price, $cost, $quantity, $image, $thumbnail) = $result->fetch_row() )
+{
+  echo "
+      <tr>
+        <td>$id</td>
+        <td><img src='$thumbnail' alt='thumbnail' class='product-thumb' /></td>
+        <td>$name</td>
+        <td>$description</td>
+        <td class='text-right'>$$price</td>
+        <td class='text-right'>$$cost</td>
+        <td class='text-right'>$quantity</td>
+      </tr>
+  ";
+}
+
+$page_foot = <<<END_OF_PAGE_FOOT
+
+    </tbody>
+  </table>
+</div>
+
+END_OF_PAGE_FOOT;
+
+echo $page_foot;
 
 include 'includes/footer.php';
 
